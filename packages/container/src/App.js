c2776dca69bece8619df60fd66229b8184e840dd
girordo <tsgiroldo@gmail.com>
2023-11-26 17:08:31 -0300

    fix(Header, mkt/App, co/App): fixing css colision classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
-    <div>
-      <h1>Hiii there!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
